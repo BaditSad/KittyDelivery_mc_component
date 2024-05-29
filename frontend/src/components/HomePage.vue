d7c1c9c72ed4d8f8dbb94c5dbab5ca402b724913
KwiiKss <lrent.kevin@gmail.com>
2024-05-29 17:13:21 +0200

    Mise en palce de la page dev avec css

--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -2,13 +2,32 @@
 <style src="../stylesheets/styles.css" scoped></style>
 
 <template>
-  <div class="hello">
-    <h1>{{ messageDuBack }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-    </p>
+  <header>
+    <h1>Kitty Delivery</h1>
+  </header>
+  <div class="container">
+    <main>
+      <section class="components">
+        <h2>Liste des composants disponibles :</h2>
+        <div class="component-list">
+          <div class="component">
+            <div class="component-image"></div>
+            <button>Télécharger</button>
+          </div>
+        </div>
+      </section>
+      <aside class="profile">
+        <h2>Mon profil</h2>
+        <br>
+        NOM
+        <br>
+        PRENOM
+        <br>
+        ETC
+        <br>
+        <button>Modifier</button>
+        <button>Supprimer</button>
+      </aside>
+    </main>
   </div>
 </template>
-
-
