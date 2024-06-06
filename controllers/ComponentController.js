a452e49f9e1c0eb7de0cac972258bd05ab56da2d
KubArthur <arthur.kubiak@viacesi.fr>
2024-06-06 15:46:23 +0200

    feature(update ComponentController)

--- controllers/ComponentController.js
+++ controllers/ComponentController.js
@@ -7,6 +7,9 @@ const Component = require("../models/component");
 router.get("/components", async (res) => {
   try {
     const components = await Component.find();
+    if (!components) {
+      return res.status(404).json({ message: "Aucun composant trouvé !" });
+    }
     res.json(components);
   } catch (error) {
     res.status(500).json({ message: error.message });
