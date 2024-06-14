1d66279b1e1326c6c3497e66297f535c661d267c
KubArthur <arthur.kubiak@viacesi.fr>
2024-06-14 14:07:11 +0200

    feature(add pagination)

--- controllers/ComponentController.js
+++ controllers/ComponentController.js
@@ -5,11 +5,26 @@ const Component = require("../models/component");
 
 router.get("/", async (req, res) => {
   try {
-    const components = await Component.find();
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    const components = await Component.find().skip(skip).limit(limit);
+
+    const totalComponents = await Component.countDocuments();
+
     if (!components) {
-      return res.status(404).json({ message: "Components not found!" });
+      return res.status(404).json({ message: "Not found" });
     }
-    res.json(components);
+
+    if (components.length === 0) {
+      return res.status(201).json({ message: "Empty" });
+    }
+
+    res.status(201).json({
+      totalPages: Math.ceil(totalComponents / limit),
+      components: components,
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
