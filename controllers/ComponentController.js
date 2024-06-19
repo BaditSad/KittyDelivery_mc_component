f3e2bd7c72aa9f5b3c82925e38acb25f2ad70678
KubArthur <arthur.kubiak@viacesi.fr>
2024-06-19 10:13:30 +0200

    feature(add "/" post, delete endpoint)

--- controllers/ComponentController.js
+++ controllers/ComponentController.js
@@ -29,3 +29,33 @@ router.get("/", async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
+
+router.post("/", async (req, res) => {
+  try {
+    const component = new Component(req.body);
+
+    if (!component) {
+      return res.status(404).json({ message: "Not found" });
+    }
+
+    await component.save();
+
+    res.status(201).json({ message: "Item posted" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.delete("/:componentId", async (req, res) => {
+  try {
+    const component = await Component.findByIdAndDelete(req.params.menuId);
+
+    if (!component) {
+      return res.status(404).json({ message: "Not found!" });
+    }
+
+    res.status(201).json({ message: "Item deleted" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
